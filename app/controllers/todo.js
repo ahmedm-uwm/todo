873bf9e78d35d767a0f415547826678c549887eb
Mohiyuddin Ahmed <ahmedm@uwm.edu>
2016-10-23 12:35:04 -0500

    without mongoose

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -5,5 +5,47 @@ var express = require('express'),
 module.exports = function (app) {
   	app.use('/api', router);  
 
-	
+	router.route('/chirps')
+
+	.get(function (req, res) {
+			logger.log("Get all chirps","verbose");
+            res.status(200).json({msg: "Get all chirps"});
+		})
+
+		.post(function(req, res){
+			logger.log("Create a chirps","verbose");
+			res.status(201).json({msg: "Create a chirps"});
+		})
+
+		.put(function(req, res){
+			logger.log("Update a chirps","verbose");
+			res.status(201).json({msg: "Update a chirps"});
+		});
+
+		router.route('/chirps/:id')	
+
+		.get(function (req, res) {
+			logger.log("Get a chirps","verbose");
+            res.status(200).json({msg: "Get a chirps"});
+		})
+
+		.delete(function (req, res) {
+			logger.log("Delete a chirps","verbose");
+            res.status(200).json({msg: "Delete a chirps"});
+		});
+
+		router.route('/chirps/userChirps/:id')	
+		
+		.get(function (req, res) {
+			logger.log("Get a user’s chirps","verbose");
+            res.status(200).json({msg: "Get a user’s chirps"});
+		});
+
+		router.route('/chirps/like/:id')	
+		
+		.put(function(req, res){
+			logger.log("Like a chirp","verbose");
+			res.status(201).json({msg: "Like a chirp"});
+		});
+
 }
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,11 +8,50 @@ module.exports = function (app) {
 	router.route('/users')	
 
 		.get(function (req, res) {
-			logger.log("Get all users","verbose");					res.status(200).json({msg: "GET all users"});
+			logger.log("Get all users","verbose");
+            res.status(200).json({msg: "GET all users"});
 		})
 
 		.post(function(req, res){
 			logger.log("Create a users","verbose");
 			res.status(201).json({msg: "Create a user"});
+		})
+
+		.put(function(req, res){
+			logger.log("Update a users","verbose");
+			res.status(201).json({msg: "Update a user"});
+		});
+
+		router.route('/users/:id')	
+
+		.get(function (req, res) {
+			logger.log("Get a users","verbose");
+            res.status(200).json({msg: "GET a user"});
+		})
+
+		.delete(function (req, res) {
+			logger.log("Delete a user","verbose");
+            res.status(200).json({msg: "Delete a user"});
+		});
+
+		router.route('/users/screenName/:name')
+
+		.get(function (req, res) {
+			logger.log("Get a user based on screen name","verbose");
+            res.status(200).json({msg: "Get a user based on screen name"});
+		});
+
+		router.route('/users/followedChirps/:id')
+		
+		.get(function (req, res) {
+			logger.log("Get the chirps of the users a user follows","verbose");
+            res.status(200).json({msg: "Get the chirps of the users a user follows"});
+		});
+
+		router.route('/users/follow/:id')
+
+		.put(function(req, res){
+			logger.log("Follow a user","verbose");
+			res.status(201).json({msg: "Follow a user"});
 		});
 }
